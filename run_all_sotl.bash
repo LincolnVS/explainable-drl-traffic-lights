7f4983ee99fb418553907fab70597931c00dad80
Lincoln V Schreiber <lincolnschreiber@gmail.com>
2021-05-27 14:22:54 -0300

    feat(bash): add dataset var

--- run_all_sotl.bash
+++ run_all_sotl.bash
@@ -3,6 +3,7 @@ green_v=(10 15 20 25 30)
 red_v=(20 25 30 35 40)
 
 file_name="log/sotl/real1x1.log"
+dataset_name="datasets/real_1x1/config.json"
 
 echo "...Starting all sotl possibilities..." | tee $file_name
 for mgt in "${min_green_time[@]}"
@@ -12,7 +13,7 @@ do
         for rv in "${min_green_time[@]}"
         do
             echo "$mgt $gv $rv" | tee -a $file_name
-            python run_sotl.py datasets/real_1x1/config_2.json --green_time $mgt --green_v $gv --red_v $rv | tee -a $file_name
+            python run_sotl.py $dataset_name --green_time $mgt --green_v $gv --red_v $rv | tee -a $file_name
             printf "\n" | tee -a $file_name
         done
     done
