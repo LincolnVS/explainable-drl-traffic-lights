06bbc3923dfcdf9fcc05a28414a5aa977cd62623
Lincoln V Schreiber <lincolnschreiber@gmail.com>
2021-09-06 13:27:18 -0300

    cyclic dqn

---
+++ scripts/run_10_my_dqn.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+file_name="log/my_dqn_run.log"
+
+info_file="agent/configs_mdqn/"
+dataset_name="envs/real_1x1/config_cluster2.json"
+
+min_test=1
+max_test=11
+
+echo "...Starting all mydqn..." | tee $file_name
+for i in $(seq $min_test $max_test)
+do
+   echo "Running $i/$max_test, 13.json"  | tee -a $file_name
+   python run_my_dqn.py $dataset_name --info_file $info_file"13.json" | tee -a $file_name
+   printf "\n" | tee -a $file_name
+done   
+
+python visualization/view-all.py "log/my_dqn/" | tee -a $file_name
+
