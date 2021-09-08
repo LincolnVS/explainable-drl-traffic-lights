4454e52846d70dba5c835d81d56ba94f62158c30
Lincoln V Schreiber <lincolnschreiber@gmail.com>
2021-09-08 16:53:46 -0300

    tosfb to cluster

---
+++ scripts/run_10_tosfb.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+file_name="log/tosfb.log"
+
+dataset_name="envs/real_1x1/config_cluster.json"
+
+min_test=1
+max_test=10
+
+echo "...Starting 10 tosfb..." | tee $file_name
+for i in $(seq $min_test $max_test)
+do
+   echo "Running $i/$max_test"  | tee -a $file_name
+   python run_new_dqn.py $dataset_name | tee -a $file_name
+   printf "\n" | tee -a $file_name
+done   
